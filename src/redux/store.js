4ad9446aabfe856351ed975510d2a2db406b59b2
Chinedu Ekene Okpala <Nedu63ima@gmail.com>
2019-05-28 00:10:45 +0000

    removed redux devtool

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,14 +8,7 @@ const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
 
 export function configureStore(initialState) {
-  const store = createStore(
-    reducers,
-    initialState,
-    compose(
-      applyMiddleware(...middlewares),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+  const store = createStore(reducers, initialState, compose(applyMiddleware(...middlewares)));
 
   sagaMiddleware.run(sagas);
 
